d88be792056c82f3b7f6b4655b191bdaf078ed8e
TomiiPorrini <Tomasjaja15@hotmail.com>
2022-08-25 21:31:48 -0300

    agregado de base de datos

--- homebanking/api/views.py
+++ homebanking/api/views.py
@@ -5,7 +5,7 @@ from .serializers import SucursalesSerializer
 from .serializers import PrestamosSerializer
 from .serializers import SaldoSerializer
 from .models import Sucursal
-from prestamos.models import Prestamos 
+from prestamos.models import Prestamos
 from cuentas.models import Cuenta
 from clientes.models import Cliente
 from empleados.models import Empleado
@@ -15,14 +15,6 @@ from rest_framework.response import Response
 from rest_framework import status
 
 
-
-
-
-
-
-
-
-
 class SucursalesLists(APIView):
     def get(self, request):
         sucursales = Sucursal.objects.all()
@@ -30,71 +22,74 @@ class SucursalesLists(APIView):
         if sucursales:
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
-    
+
+
 class Prestamos(APIView):
     def delete(self, request, pk):
-        #Buscamos el prestamos del cliente
-        try: 
-            prestamos = Prestamos.objects.filter(dni = pk).first()
-            #Buscamos el cliente para buscar su cuenta
-            cliente = Cliente.objects.filter(customer_DNI = pk).first()
+        # Buscamos el prestamos del cliente
+        try:
+            prestamos = Prestamos.objects.filter(dni=pk).first()
+            # Buscamos el cliente para buscar su cuenta
+            cliente = Cliente.objects.filter(customer_DNI=pk).first()
             clienteId = cliente.customer_id
-            cuenta = Cuenta.objects.filter(customer_id = clienteId).first()
+            cuenta = Cuenta.objects.filter(customer_id=clienteId).first()
             cuenta.balance -= prestamos.valor
             prestamos.delete()
             return Response(status=status.HTTP_204_NO_CONTENT)
-        except: 
+        except:
             return Response('Algo fallo', status=status.HTTP_400_BAD_REQUEST)
-    
+
+
 class PrestamoSucursalList(APIView):
     def get(self, request, sucursal_id):
         try:
-            clientes = Cliente.objects.filter(branch_id = sucursal_id)
+            clientes = Cliente.objects.filter(branch_id=sucursal_id)
             dnis = []
             for ppl in clientes:
                 dnis.append(ppl.customer_DNI)
             losPrestamos = []
             for dni in dnis:
-                losPrestamos.extend(list(Prestamos.objects.filter(dni = int(dni))))
+                losPrestamos.extend(
+                    list(Prestamos.objects.filter(dni=int(dni))))
             if losPrestamos:
-                serializer = PrestamosSerializer(losPrestamos,many=True)
+                serializer = PrestamosSerializer(losPrestamos, many=True)
                 return Response(serializer.data, status=status.HTTP_200_OK)
         except:
             return Response('Algo fallo', status=status.HTTP_400_BAD_REQUEST)
 
-<<<<<<< HEAD
+
+
 class Create_prestamo(APIView):
     def post(self, request):
-      try: 
-        username = request.user
-        if Empleado.objects.filter(employee_dni = username.username):
-           serializer = PrestamosSerializer(data = request.data)
-        if serializer.is_valid():
-          serializer.save()
-        else: 
-            return Response('Datos incorrectos', status=status.HTTP_400_BAD_REQUEST)
-        cliente = Cliente.objects.filter(customer_DNI = request.data['dni']).first()
-        clienteId = cliente.customer_id
-        cuenta = Cuenta.objects.filter(customer_id = clienteId).first()
-        cuenta.balance += int(request.data['valor'])
-        cuenta.save()
-        return Response(serializer.data)
-      except: 
-        return Response('Fallo')         
-=======
+        try:
+            username = request.user
+            if Empleado.objects.filter(employee_dni=username.username):
+                serializer = PrestamosSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+            else:
+                return Response('Datos incorrectos', status=status.HTTP_400_BAD_REQUEST)
+            cliente = Cliente.objects.filter(
+                customer_DNI=request.data['dni']).first()
+            clienteId = cliente.customer_id
+            cuenta = Cuenta.objects.filter(customer_id=clienteId).first()
+            cuenta.balance += int(request.data['valor'])
+            cuenta.save()
+            return Response(serializer.data)
+        except:
+            return Response('Fallo')
 
 
 
 class SaldoDetails(APIView):
     def get(self, request, pk):
-        #Buscamos el prestamos del cliente
-        try: 
-            #Buscamos el cliente para buscar su cuenta
-            saldo = Cliente.objects.filter(customer_DNI = pk).first()
+        # Buscamos el prestamos del cliente
+        try:
+            # Buscamos el cliente para buscar su cuenta
+            saldo = Cliente.objects.filter(customer_DNI=pk).first()
             clienteId = saldo.customer_id
-            cuenta = Cuenta.objects.filter(customer_id = clienteId).first()
+            cuenta = Cuenta.objects.filter(customer_id=clienteId).first()
             return Response(cuenta.balance, status=status.HTTP_204_NO_CONTENT)
-        except: 
+        except:
             return Response('Algo fallo', status=status.HTTP_400_BAD_REQUEST)
             
->>>>>>> main
